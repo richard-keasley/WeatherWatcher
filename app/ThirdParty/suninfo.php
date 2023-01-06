8ad1a832be0e26ae578adea32795a1a9bf855810
richard-keasley <67803679+richard-keasley@users.noreply.github.com>
2023-01-06 22:49:41 +0000

    Update suninfo.php

--- app/ThirdParty/suninfo.php
+++ app/ThirdParty/suninfo.php
@@ -1,3 +1,11 @@
-<?php namespace App\ThirdParty;
-class suninfo { 
-static function load($timestamp=null, $latitude=0, $longitude=0) {$path = __DIR__ . '/suninfo-1.0/suninfo.php';include $path;return new \basecamp\suninfo($timestamp, $latitude, $longitude);}}
+<?php namespace App\ThirdParty;
+
+class suninfo { 
+
+static function load($timestamp=null, $latitude=0, $longitude=0) {
+$path = __DIR__ . '/suninfo-1.0/suninfo.php';
+include $path;
+return new \basecamp\suninfo($timestamp, $latitude, $longitude);
+}
+
+}
