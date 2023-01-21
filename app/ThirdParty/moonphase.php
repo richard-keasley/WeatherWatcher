44e3850f9e2cd82b614c1a26c48237ee8c940397
richard-keasley <67803679+richard-keasley@users.noreply.github.com>
2023-01-21 17:48:06 +0000

    moon phase

--- app/ThirdParty/moonphase.php
+++ app/ThirdParty/moonphase.php
@@ -1,39 +1,40 @@
-<?php namespace App\ThirdParty;
-
-class moonphase {
-
-static $version = '2.1.0';
-static $date = '2022-05-20';
-	
-static function load($datetime=null) {
-	$path = __DIR__ . '/php-moon-phase-' . self::$version;
-	include "{$path}/src/MoonPhase.php";
-	return new \Solaris\MoonPhase($datetime);
-}
-
-static function img($moonphase) {
-	\helper('html');
-	
-	$phase = $moonphase->getPhase();
-	$phase_name = $moonphase->getPhaseName();
-
-	$key = $phase - floor($phase); // get decimal part
-	$key = round($key * 16); // 16 images 
-	$img = [
-		'src' => site_url(sprintf('app/moon/%02d.png', $key)),
-		'alt' => $phase_name,
-		'style' => "width: 6em;"
-	];
-	$attrs = [
-		'title' => $phase_name,
-		'style' => "
-			background: #1b1b30;
-			display: inline-block;
-			padding:.4em;
-			"
-	];
-	return sprintf('<figure %s>%s</figure>', \stringify_attributes($attrs), \img($img));
-}
-
-}
-
+<?php namespace App\ThirdParty;
+
+class moonphase {
+
+static $version = '2.1.0';
+static $date = '2022-05-20';
+	
+static function load($datetime=null) {
+	$path = __DIR__ . '/php-moon-phase-' . self::$version;
+	include_once "{$path}/src/MoonPhase.php";
+	return new \Solaris\MoonPhase($datetime);
+}
+
+static function img($moonphase) {
+	\helper('html');
+	
+	$phase = $moonphase->getPhase();
+	$phase_name = $moonphase->getPhaseName();
+
+	$key = $phase - floor($phase); // get decimal part
+	$key = round($key * 16) % 16; // 16 images
+
+	$img = [
+		'src' => site_url(sprintf('app/moon/%02d.png', $key)),
+		'alt' => $phase_name,
+		'style' => "width: 6em;"
+	];
+	$attrs = [
+		'title' => $phase_name,
+		'style' => "
+			background: #1b1b30;
+			display: inline-block;
+			padding:.4em;
+			"
+	];
+	return sprintf('<figure %s>%s</figure>', \stringify_attributes($attrs), \img($img));
+}
+
+}
+
--- app/Views/widgets/moon.php
+++ app/Views/widgets/moon.php
@@ -1,37 +1,37 @@
-<section><?php
-$datetime = null;#new \datetime('10 Jan 2023 23:09');
-$moonphase = \App\ThirdParty\moonphase::load($datetime);
-helper('html');
-
-$timestamps = [
-	'this_new'  => $moonphase->getPhaseNewMoon(),
-	'this_full' => $moonphase->getPhaseFullMoon(),
-	'next_new'  => $moonphase->getPhaseNextNewMoon(),
-	'next_full' => $moonphase->getPhaseNextFullMoon(),
-];
-asort($timestamps);
-
-$phase = $moonphase->getPhase();
-$phase_name = $moonphase->getPhaseName();
-
-$tbody = []; 
-$now = time();
-$datetime = new \DateTime;
-foreach($timestamps as $key=>$timestamp) {
-	if($timestamp>=$now && count($tbody)<2) {
-		$arr = explode('_', $key);
-		$label = "Next {$arr[1]} moon";
-		$datetime->setTimestamp($timestamp);
-		$tbody[] = [$label, $datetime->format('j M Y H:i')];
-	}
-}
-
-$tbody[] = ['Phase', sprintf('%s (%s%%)', $phase_name, round($phase * 100))];
-
-echo \App\ThirdParty\moonphase::img($moonphase);
-
-$table = new \CodeIgniter\View\Table();
-$table->autoHeading = false;
-echo $table->generate($tbody);
-
+<section><?php
+$datetime = null;
+$moonphase = \App\ThirdParty\moonphase::load($datetime);
+helper('html');
+
+$timestamps = [
+	'this_new'  => $moonphase->getPhaseNewMoon(),
+	'this_full' => $moonphase->getPhaseFullMoon(),
+	'next_new'  => $moonphase->getPhaseNextNewMoon(),
+	'next_full' => $moonphase->getPhaseNextFullMoon(),
+];
+asort($timestamps);
+
+$phase = $moonphase->getPhase();
+$phase_name = $moonphase->getPhaseName();
+
+$tbody = []; 
+$now = time();
+$datetime = new \DateTime;
+foreach($timestamps as $key=>$timestamp) {
+	if($timestamp>=$now && count($tbody)<2) {
+		$arr = explode('_', $key);
+		$label = "Next {$arr[1]} moon";
+		$datetime->setTimestamp($timestamp);
+		$tbody[] = [$label, $datetime->format('j M Y H:i')];
+	}
+}
+
+$tbody[] = ['Phase', sprintf('%s (%s%%)', $phase_name, round($phase * 100))];
+
+echo \App\ThirdParty\moonphase::img($moonphase);
+
+$table = new \CodeIgniter\View\Table();
+$table->autoHeading = false;
+echo $table->generate($tbody);
+
 ?></section>
