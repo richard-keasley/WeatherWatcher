4d3906d52af63b6ebafe99267116c75e33b26953
Richard Keasley <rk@base-camp.org.uk>
2026-06-29 10:24:49 +0100

    update mooninfo

--- app/ThirdParty/mooninfo.php
+++ app/ThirdParty/mooninfo.php
@@ -2,7 +2,7 @@
 
 class mooninfo {
 
-const version = '1.0.2';
+const version = '1.0.3';
 const date = '2026-06-23';
 	
 static $registered = false;
--- app/Views/includes/moon.php
+++ app/Views/includes/moon.php
@@ -3,7 +3,10 @@
 \App\ThirdParty\mooninfo::autoload();
 $mooninfo = new \basecamp\mooninfo;
 
-$items = [];
+$items = [
+	'Phase' => ['Phase', sprintf('%s (%u%%)', $mooninfo->phase_name, round($mooninfo->illumination * 100))],
+];
+
 $timestamps = [
 	'this_new'  => $mooninfo->getPhaseNewMoon(),
 	'this_full' => $mooninfo->getPhaseFullMoon(),
@@ -13,23 +16,21 @@ $timestamps = [
 asort($timestamps);
 $datetime = new \DateTime;
 // future lunar events
+$count = 0;
 foreach($timestamps as $key=>$timestamp) {
 	if($timestamp<=$mooninfo->timestamp) continue; // in the past
 	$arr = explode('_', $key);
 	$label = "Next {$arr[1]} moon";
 	$datetime->setTimestamp((int) $timestamp);
 	$items[] = [$label, $datetime->format('j M Y H:i')];
-	if(count($items)>1) break; // all done
+	$count++;
+	if($count==2) break; // all done
 }
-//  current phase
-$phase = round($mooninfo->phase * 100);
-$items[] = ['Phase', "{$mooninfo->phase_name} ({$phase}%)"];
 // moon name
 $name = $mooninfo->name;
 $blue = $mooninfo->blue;
 if($blue) $name .= " (blue {$blue})";
-$items[] = ['Name', $name];
-
+if($name) $items[] = ['Name', $name];
 
 $format = '<div style="width:5em;background:#112;padding:0.5em;">%s</div>';
 printf($format, $mooninfo->image);
